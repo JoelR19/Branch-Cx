8a976307c958d83a049479bc8f17f819c4760c98
JesuisDv <jdfrancov2020@gmail.com>
2025-04-15 13:35:07 -0500

    Se mejoró el banner de envíos con un nuevo diseño y se ajustó el intervalo de mensajes para una mejor experiencia del usuario.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const msjBanner = ["Envios gratis por compras superiores a $299.900", "¡Devoluciones gratis!"];
-let index = 0;
+const msjBanner = [
+    "Envios gratis por compras superiores a $299.900 🚚",
+    "¡Devoluciones gratis!"
+  ];
 
-const contenedor = document.getElementById("bannerHead");
+  const msjBanner2 = [
+    "Términos y Condiciones",
+    "Términos y Condiciones"
+  ];
 
-const intervalo = setInterval(() => {
-    contenedor.textContent = msjBanner[index];
-    index = (index + 1)% msjBanner.length;  
-},2000);
+  let index = 0;
+  
+  const contenedor = document.getElementById("bannerHead");
+  const contenedor2 = document.getElementById("bannerHead2");
+
+  
+  // Mostrar el primer mensaje apenas carga
+  contenedor.textContent = msjBanner[index];
+  index = (index + 1) % msjBanner.length;
+  
+  setInterval(() => {
+    contenedor.classList.add("translate-x-4", "blur-sm", "opacity-0");
+  
+    setTimeout(() => {
+      contenedor.textContent = msjBanner[index];
+      index = (index + 1) % msjBanner.length;
+      contenedor.classList.remove("translate-x-4", "blur-sm", "opacity-0");
+    }, 250);
+  }, 4000);
+  
+  
